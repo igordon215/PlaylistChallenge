042d4b2a0980a2c81900173ce6aceff19d9bff20
igordon215 <igordon215@gmail.com>
2024-07-12 08:48:41 -0400

    lab done

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,30 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int selectUp = 0;
+        int selectDown = 0;
+
+        if(startIndex == 0 && this.playList[this.playList.length-1].equals(selection)){
+            return 1;
+        }
+        for (int i = startIndex; i < this.playList.length; i++){
+            if(!this.playList[i].equals(selection)) {
+                selectDown++;
+            } else break;
+            //System.out.println(selection);
+        }
+
+        for (int i = startIndex; i >= 0; i--){
+            if (!this.playList[i].equals(selection)){
+                selectUp++;
+            } else break;
+            //System.out.println(selection);
+        }
+
+        if (selectDown < selectUp){
+            return selectDown;
+        } else return selectUp;
+
     }
+
 }
